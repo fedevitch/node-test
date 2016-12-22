e44312624e0cdcd91621b4262baa98275b614b11
fedevitch <lyubomyr.fedevitch@eliftech.com>
2016-12-22 15:06:00 +0200

    upd index

--- client/index.jsx
+++ client/index.jsx
@@ -4,12 +4,23 @@ import ReactDOM from 'react/lib/ReactDOM';
 class Main extends React.Component{
 	constructor(props) {
 		super(props);
+		console.log("constructor of <Main/> executed successfully");
 	}
 
 	render(){
 		return(
 			<div>
 				<center><h3>Hello from boilerplate</h3></center>
+				<h4>node-test</h4>
+
+				<p>sample of project</p>
+				
+				Contains:
+				<ul>
+					<li>nodejs server</li>
+					<li>react boilerplate</li>
+					<li>sample of unit tests, jshint(codestyle checker) and istanbul(coverage checker)</li>
+				</ul>
 			</div>
 		);
 	}

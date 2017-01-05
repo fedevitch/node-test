19c66ca2c4eea8196835fd704b2b6935961717a9
fedevitch <lyubomyr.fedevitch@eliftech.com>
2017-01-05 14:38:43 +0200

    log15

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ var startServer = function(serverFilePath){
 
 var startMainServer = function () {
     console.log(Date().toString(), ": starting server");
-    console.log("build-00014");
+    console.log("build-00015");
     startServer('./server.js');
 };
 
